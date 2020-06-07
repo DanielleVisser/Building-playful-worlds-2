283ebd95cc0b436a92ce68f744e9eaff2a36cf81
Daniëlle <moontowhee@gmail.com>
2020-06-07 21:01:21 +0200

    Update level switch

---
+++ Virtual game/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Loading level with Index
+            SceneManager.LoadScene(index);
+
+            //loading level with scene name
+            //SceneManager.LoadScene(levelName);
+
+            //Restart lvl
+            //Scenemanager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}

05bc807a6b189e7f9aa2822775b8244d40af3f07
Daniëlle <moontowhee@gmail.com>
2020-06-16 15:28:15 +0200

    Final Update

---
+++ Virtual game/Assets/MenuControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    //public void ButtonStart()
+    //{
+        //SceneManager.LoadScene(1);
+    //}
+
+    //public void ButtonQuit()
+    //{
+        //Application.Quit();
+    //}
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
